0df49e518bffa5bde42af1c718db03513c75f7c9
cornel <ghita.cornel@gmail.com>
2020-12-05 14:48:38 +0200

    better

--- src/main/java/main/model/Person.java
+++ src/main/java/main/model/Person.java
@@ -5,6 +5,9 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
@@ -16,5 +19,21 @@ public class Person {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
